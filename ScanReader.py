8393a1baa1487ba5bc27743818721b818b59b71f
JackSnyder427 <3147104s@student.gla.ac.uk>
2026-07-31 22:10:41 -0400

    Pytesseract text detection; Contrast increase for greater accuracy; Still highly inaccurate

--- ScanReader.py
+++ ScanReader.py
@@ -1,9 +1,24 @@
-import pymupdf
+import fitz
+import pytesseract
+from PIL import Image, ImageOps
 
-document = pymupdf.open("eob.pdf")
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-for page_number, page in enumerate(document):
-    text = page.get_text()
+pdf = fitz.open("EOBs/MCEOBExample.pdf")
 
-    print(f"--- PAGE {page_number + 1} ---")
+for page_num in range(len(pdf)):
+    page = pdf.load_page(page_num)
+
+    # Render page as an image
+    pix = page.get_pixmap(dpi=600)
+
+    image = pix.pil_image()
+
+    image = ImageOps.grayscale(image)
+    image = ImageOps.autocontrast(image)
+
+    # OCR the image
+    text = pytesseract.image_to_string(image)
+
+    print(f"----- Page {page_num + 1} -----")
     print(text)
